d537695d8b5d031f1134bcb86c2490cc2b9eb15a
aneesh <aneeshpanoli@gmail.com>
2020-04-23 03:00:14 -0700

    commit

--- PyzymeGameplay.cs
+++ PyzymeGameplay.cs
@@ -19,33 +19,45 @@ public class PyzymeGameplay
         start_obj = obj_transform;
         }
         else if (state.Equals("Ended")){
+            
         // Debug.Log(obj_transform.gameObject.name);
-        if (start_obj && obj_transform && start_obj != obj_transform){
-            Debug.Log("Swapped");
-            Vector2 temp_pos = start_obj.localPosition;
-            Debug.Log(temp_pos);
-            Vector2 list_ind = get_arr_ind_from_position(temp_pos);
-            Debug.Log(list_ind);
-            GridMaker.tiles[(int)list_ind.x][(int)list_ind.y] = obj_transform.gameObject;
+            if (start_obj && obj_transform ){
+                Vector2 temp_pos = start_obj.localPosition;
+                if (!is_swappable(temp_pos, obj_transform.localPosition)){
+                    return;
+                }
+                Debug.Log("Swapped");
+                Debug.Log(temp_pos);
+                Vector2 list_ind = get_arr_ind_from_position(temp_pos);
+                Debug.Log(list_ind);
+                GridMaker.tiles[(int)list_ind.x][(int)list_ind.y] = obj_transform.gameObject;
 
-            list_ind = get_arr_ind_from_position(obj_transform.localPosition);
-            Debug.Log(obj_transform.localPosition);
-            Debug.Log(list_ind);
-            GridMaker.tiles[(int)list_ind.x][(int)list_ind.y] = start_obj.gameObject;
+                list_ind = get_arr_ind_from_position(obj_transform.localPosition);
+                Debug.Log(obj_transform.localPosition);
+                Debug.Log(list_ind);
+                GridMaker.tiles[(int)list_ind.x][(int)list_ind.y] = start_obj.gameObject;
 
-            start_obj.localPosition = obj_transform.localPosition;
-            obj_transform.localPosition = temp_pos;
- 
-            start_obj = null;
-            check_colwords();
-            // print_board();
-        }
+                start_obj.localPosition = obj_transform.localPosition;
+                obj_transform.localPosition = temp_pos;
+    
+                start_obj = null;
+                check_colwords();
+                // print_board();
+            }
       }
 }
 
 private static Vector2 get_arr_ind_from_position(Vector2 pos){
     return new Vector2((int)pos.x + GridMaker.dim/2, 
     (int)GridMaker.dim/2 - pos.y);
+}
+
+private static bool is_swappable(Vector2 pos1, Vector2 pos2){
+    if ((Mathf.Abs(pos1.x - pos2.x) == 1 || Mathf.Abs(pos1.y - pos2.y) == 1)
+     && (pos1.x == pos2.x || pos1.y == pos2.y)){
+        return true;
+    }
+    return false;
 }
 
 private static void print_board(){
